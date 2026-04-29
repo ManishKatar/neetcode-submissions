5f549e04b5fd33030f54a05de12ce7cc11408898
ManishKatar <144347147+ManishKatar@users.noreply.github.com>
2026-04-29 12:56:19 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        // check the length of the two strings different means no anagram
+        if(s.length() != t.length() ){
+            return false;
+
+        }
+
+        int count[26]={0};
+
+        for(int i=0 ; i<s.length(); i++){
+            count[s[i]-'a']++;
+            count[t[i]-'a']--;
+
+        }
+
+        for(int i=0; i<26; i++){
+            if( count[i] != 0 ){
+                return false;
+
+
+            }
+
+       
+
+
+        }
+
+
+     return true;
+        
+    }
+    
+};
